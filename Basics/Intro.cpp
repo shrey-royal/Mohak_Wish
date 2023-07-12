b550d648a8a583881dae80c7da2282aff45cc38e
Shrey K <srkkadia@gmail.com>
2023-07-12 17:54:41 +0530

    First.cpp

---
+++ Basics/Intro.cpp
@@ -0,0 +1,41 @@
+/*
+Cpp: C Plus Plus
+
+C++ is a general purpose programming language and widely used now a days for competitive programming.
+
+> Object Oriented Programming Language supported by C++.
+*/
+
+#include<iostream>  // header file
+using namespace std;  // namespace created as std
+
+int main()  // main function
+{
+    cout << "Hello World";  // cout is used to print the output
+    return 0;  // return 0 if program is successfully executed
+}
+
+/*
+iostream -> input output stream (istream + ostream)
+istream -> input stream (cin)
+ostream -> output stream (cout)
+
+cout -> console output stream -> used to print the output
+> cout uses insertion operator (<<) to print the output
+
+cin -> console input stream -> used to take input from the user
+> cin uses extraction operator (>>) to take input from the user
+
+
+Difference between void main() and int main():
+
+> void main() is not a standard way to write the main function
+> int main() is a standard way to write the main function
+
+> void main() is not returning any value
+> int main() is returning 0 if program is successfully executed
+
+> void main() is not taking any parameter
+> int main() is taking two parameters (int argc, char* argv[])
+
+*/
