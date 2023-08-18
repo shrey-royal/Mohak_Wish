723de2bad310e3ba5ae6df1cd03bfbcc81d3a0c4
Shrey K <srkkadia@gmail.com>
2023-08-18 16:46:38 +0530

    Multiple Inheritance

---
+++ CPP/OOPs/Inheritance/Multiple.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+class Architects {
+    public:
+        Architects() {
+            cout << "Architects" << endl;
+        }
+
+        void Imagine() {
+            cout << "Imagine the design..." << endl;
+        }
+};
+
+class CivilEngineers : public Architects {
+    public:
+        CivilEngineers() {
+            cout << "CivilEngineers" << endl;
+        }
+
+        void Build() {
+            cout << "Build the design..." << endl;
+        }
+};
+
+class InteriorDesigners : public Architects {
+    public:
+        InteriorDesigners() {
+            cout << "InteriorDesigners" << endl;
+        }
+
+        void Decorate() {
+            cout << "Decorate the design..." << endl;
+        }
+};
+
+int main() {
+    CivilEngineers evil;
+    InteriorDesigners id;
+    
+    evil.Imagine();
+    id.Imagine();
+
+    evil.Build();
+    id.Decorate();
+
+
+    return 0;
+}
+
+/*
+
+1. **Employee Hierarchy with Skills:**
+   Design a class hierarchy for an employee management system. Each employee can be of different types (Manager, Engineer, Salesperson, etc.) and possess multiple skills (Programming, Sales, Communication, etc.). Implement the hierarchy using multiple inheritance, ensuring that each employee type inherits from both a base Employee class and a Skills class. Handle any potential conflicts or ambiguity arising from inheriting shared attributes or methods.
+
+2. **Shape Hierarchy with Drawing:**
+   Create a shape hierarchy that includes geometric shapes like Circle, Square, and Triangle. Implement a Drawing class that simulates drawing these shapes on a canvas. Utilize multiple inheritance to achieve this, allowing each shape to inherit from both the Shape base class and the Drawing class to provide drawing functionality.
+
+3. **Vehicle Fleet with Emission Standards:**
+   Model a vehicle fleet containing different types of vehicles (Car, Truck, Motorcycle) with varying emission standards (Euro 4, Euro 5, Euro 6). Utilize multiple inheritance to design a hierarchy that combines both the Vehicle class and the EmissionStandard class. Address any issues related to inheriting conflicting attributes or methods from the base classes.
+
+4. **Online Shopping Cart with Discounts:**
+   Develop an online shopping cart system that calculates discounts for various products based on their categories (Electronics, Clothing, Books) and ongoing promotions (Seasonal Sale, Holiday Discount). Use multiple inheritance to construct a hierarchy of classes, incorporating both the Product class and the Discount class. Manage potential challenges that arise from inheriting discount calculation logic and product attributes.
+
+5. **Animal Classification with Traits:**
+   Design a class hierarchy to categorize animals into different types (Mammals, Birds, Fish) and specify traits (Carnivore, Herbivore, Omnivore). Implement multiple inheritance to create a structure where each animal type inherits from both the Animal class and the Trait class. Handle any complications arising from inheriting conflicting methods or attributes related to animal behavior and traits.
+
+*/
