61aad004a10b58a88a29338106afb28e5ba88968
Shrey K <srkkadia@gmail.com>
2023-08-28 16:34:02 +0530

    Hybrid Inheritance

---
+++ CPP/OOPs/Inheritance/Hybrid.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class School {
+public:
+    void printSchool() {
+        cout << "School" << endl;
+    }
+};
+
+class Principle : public School {
+public:
+    void printPrinciple() {
+        cout << "Principle" << endl;
+    }
+};
+
+class Teacher : public School {
+public:
+    void printTeacher() {
+        cout << "Teacher" << endl;
+    }
+};
+
+class Student : public Teacher, public Principle {
+public:
+    void printStudent() {
+        cout << "Student" << endl;
+    }
+};
+
+int main() {
+    Student s;
+
+    s.printStudent();
+    s.printPrinciple();
+    s.printTeacher();
+    s.Teacher::printSchool();
+    
+    return 0;
+}
